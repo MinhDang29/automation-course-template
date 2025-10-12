0f6e65eceb3abebb7471cd362f8ce5e7fe9a129c
DangNguyen <67234142+dangnguyen1004@users.noreply.github.com>
2025-10-12 07:31:39 +0700

    Update URL to Google.com

--- src/test/java/com/LoginTest.java
+++ src/test/java/com/LoginTest.java
@@ -6,12 +6,10 @@ import org.testng.annotations.Test;
 import com.utils.BasicTest;
 
 public class LoginTest extends BasicTest {
-
-
     @Test()
     public void loginTest() throws Exception {
         // Launch website
-        String url = "https://automationtestpro.com/";
+        String url = "https://www.google.com/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
     }
