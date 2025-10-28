166990690d7037a8e793702ca20caee0bddbad83
Minh Dang <mdangdn29@gmail.com>
2025-10-28 18:53:44 +0700

    Lưu lại công việc trước khi chuyển sang main

--- src/main/java/com/utils/BasicTest.java
+++ src/main/java/com/utils/BasicTest.java
@@ -6,21 +6,27 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.Parameters;
 import org.openqa.selenium.edge.EdgeDriver;
 
 
+
     
 public abstract class BasicTest {
-    
+   
     public static final Logger logger = LogManager.getLogger();
     protected static WebDriver driver;
+    public static WebDriverWait wait;
+    //WebDriver driver;
     // private String driverPath;
 
     /**
@@ -34,7 +40,7 @@ public abstract class BasicTest {
         // ChromeOptions options = new ChromeOptions();
         // System.setProperty("webdriver.chrome.driver", driverPath);
         // driver = new ChromeDriver(options);
-
+         
         String browser = Constants.browser;
         
         if (browser.equalsIgnoreCase("chrome")) {
@@ -57,6 +63,7 @@ public abstract class BasicTest {
         // Maximize the browser
         driver.manage().window().maximize();
         //driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(4));
     }
 
     @AfterMethod
